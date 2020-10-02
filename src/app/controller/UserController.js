e4c1f96b9996501a5c99f3381d9ee80aeffdbb3f
fabiobbsa <fabiobbsa@outlook.com>
2020-10-02 14:12:24 -0300

    projeto até a aula 15 do fase2 módulo 2

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -17,6 +17,12 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+
+    return res.json({ OK: true });
+  }
 }
 
 export default new UserController();
