62835e26db0bd49ca1f20707f703dee354250582
Fabio <fabiobbsa>
2020-10-27 23:52:21 -0300

    tentativa de corrigir o autofix on save

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,24 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next ' }],
-  },
+    env: {
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'airbnb-base', 'prettier'
+    ],
+    // "parser": "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        // '@typescript-eslint',
+        'prettier'
+    ],
+    rules: {
+      "class-methods-use-this": "off",
+      "no-param-reassign": "off",
+      "camelcase": "off",
+      "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+    },
 };
